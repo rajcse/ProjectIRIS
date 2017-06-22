1aac1a5a116d98f0a3306f2178705c93cbafd19e
Liam Demafelix <liam@demafelix.me>
2017-06-23 01:05:24 +0800

    First

---
+++ bot.php
@@ -0,0 +1,17 @@
+<?php
+
+require "config.php";
+require "kernel.php";
+
+$token = $_REQUEST['hub_verify_token'];
+$hubVerifyToken = $facebook['verify_token'];
+$challenge = $_REQUEST['hub_challenge'];
+$accessToken = $facebook['page_access_token'];
+
+$bot = new FbBot();
+$bot->setHubVerifyToken($hubVerifyToken);
+$bot->setaccessToken($accessToken);
+echo $bot->verifyTokken($tokken,$challange);
+$input = json_decode(file_get_contents('php://input'), true);
+$message = $bot->readMessage($input);
+$textmessage = $bot->sendMessage($message);
---
+++ kernel.php
@@ -0,0 +1,99 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Psr7\Request;
+
+class FbBot
+{
+    private $hubVerifyToken = null;
+    private $accessToken = null;
+    private $token = false;
+    protected $client = null;
+
+    public function setHubVerifyToken($value)
+    {
+        $this->hubVerifyToken = $value;
+    }
+
+    public function setAccessToken($value)
+    {
+        $this->accessToken = $value;
+    }
+
+    public
+
+    function verifyToken($hub_verify_token, $challenge)
+    {
+        try {
+            if ($hub_verify_token === $this->hubVerifyToken) {
+                return $challenge;
+            }
+            else {
+                throw new Exception("Token not verified");
+            }
+        }
+
+        catch (Exception $ex) {
+            return $ex->getMessage();
+        }
+    }
+
+    public function readMessage($input)
+    {
+        try {
+            $payloads = null;
+            $senderId = $input['entry'][0]['messaging'][0]['sender']['id'];
+            $messageText = $input['entry'][0]['messaging'][0]['message']['text'];
+            $postback = $input['entry'][0]['messaging'][0]['postback'];
+            $loctitle = $input['entry'][0]['messaging'][0]['message']['attachments'][0]['title'];
+            if (!empty($postback)) {
+                $payloads = $input['entry'][0]['messaging'][0]['postback']['payload'];
+                return ['senderid' => $senderId, 'message' => $payloads];
+            }
+
+            if (!empty($loctitle)) {
+                $payloads = $input['entry'][0]['messaging'][0]['postback']['payload'];
+                return ['senderid' => $senderId, 'message' => $messageText, 'location' => $loctitle];
+            }
+
+            return ['senderid' => $senderId, 'message' => $messageText];
+        }
+
+        catch(Exception $ex) {
+            return $ex->getMessage();
+        }
+    }
+
+    public function sendMessage($input)
+    {
+        try {
+            $client = new GuzzleHttpClient();
+            $url = "https://graph.facebook.com/v2.6/me/messages";
+            $messageText = strtolower($input['message']);
+            $senderId = $input['senderid'];
+            $msgarray = explode(' ', $messageText);
+            $response = null;
+            $header = [
+                'content-type' => 'application/json'
+            ];
+
+            if (in_array('help', $msgarray)) {
+                $answer = "My name is Iris, messenger of the gods. To send a message, enter a phone number and a slash, followed by your message. For example: 09771234567/Hello, my name is Iris.";
+                $response = ['recipient' => ['id' => $senderId], 'message' => ['text' => $answer], 'access_token' => $this->accessToken];
+            }
+
+            $response = $client->post($url, ['query' => $response, 'headers' => $header]);
+
+            return true;
+        }
+
+        catch(RequestException $e) {
+            $response = json_decode($e->getResponse()->getBody(true)->getContents());
+            file_put_contents("test.json", json_encode($response));
+            return $response;
+        }
+    }
+}
