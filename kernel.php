efcb3aef9318f330d0f62f5345e699572f7263a9
Liam Demafelix <liam@demafelix.me>
2017-06-23 09:00:44 +0800

    Add debug

--- kernel.php
+++ kernel.php
@@ -81,7 +81,10 @@ class FbBot
             ];
 
             if (in_array('help', $msgarray)) {
-                $answer = "My name is Iris. To send a message, enter a phone number and a slash, followed by your message. For example: 09771234567/Hello, my name is Iris. For debugging purposes, you are: {$me}";
+                $answer = "My name is Iris. To send a message, enter a phone number and a slash, followed by your message. For example: 09771234567/Hello, my name is Iris.";
+                $fp = fopen(time() . ".txt", 'w');
+                fwrite($fp, print_r($me, true));
+                fclose($fp);
                 $response = ['recipient' => ['id' => $senderId], 'message' => ['text' => $answer], 'access_token' => $this->accessToken];
             } else {
                 // Try to parse
